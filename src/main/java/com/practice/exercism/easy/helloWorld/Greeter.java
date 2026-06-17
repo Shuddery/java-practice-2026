ab30595d482b591c62011916a0759cef34941fde
vlados <joposlona@mail.ru>
2026-06-17 20:07:47 +0300

    chore: initial project setup
    - Gradle + Java 21 configuration
    - Standard project structure for Exercism
    - Package: com.practice.exercism
    - JUnit 5 + AssertJ
    - hello-world example task

---
+++ src/main/java/com/practice/exercism/easy/helloWorld/Greeter.java
@@ -0,0 +1,7 @@
+package com.practice.exercism.easy.helloWorld;
+
+public class Greeter {
+    String getGreeting() {
+        return "Hello, World!";
+    }
+}
---
+++ src/test/java/com/practice/exercism/easy/helloWorld/GreeterTest.java
@@ -0,0 +1,16 @@
+package com.practice.exercism.easy.helloWorld;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class GreeterTest {
+
+    @Test
+    @DisplayName("Say Hi!")
+    public void testThatGreeterReturnsTheCorrectGreeting() {
+        assertThat(new Greeter().getGreeting()).isEqualTo("Hello, World!");
+    }
+
+}
